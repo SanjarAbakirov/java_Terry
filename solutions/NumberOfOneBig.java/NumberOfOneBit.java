d541742f5bf5950799caeb87afbfdc11082788cf
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-13 23:28:24 +0600

    auto commit Fri Feb 13 23:28:24 +06 2026

--- solutions/NumberOfOneBig.java/NumberOfOneBit.java
+++ solutions/NumberOfOneBig.java/NumberOfOneBit.java
@@ -1,5 +1,5 @@
 class NumberOfOneBit {
-    public static int hammingWeight(int n) {
+    public static int numberOfOneBit(int n) {
         int count = 0;
         while (n != 0) {
             count += n & 1;
@@ -8,8 +8,20 @@ class NumberOfOneBit {
         return count;
     }
 
+    // Проверочная функция (тест)
     public static void main(String[] args) {
         int n = 22;
+        int result = numberOfOneBit(n);
+        int expected = Integer.bitCount(n);
+
         System.out.println("n = " + n + " (binary: " + Integer.toBinaryString(n) + ")");
+        System.out.println("Hamming weight = " + result);
+        System.out.println("Expected = " + expected);
+
+        if (result == expected) {
+            System.out.println("Тест пройден!");
+        } else {
+            System.out.println("Тест не пройден!");
+        }
     }
 }
