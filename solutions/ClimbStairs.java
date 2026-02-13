d62e306f149d2a281906e68f2447c481c1992280
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-13 23:22:20 +0600

    auto commit Fri Feb 13 23:22:20 +06 2026

--- solutions/ClimbStairs.java
+++ solutions/ClimbStairs.java
@@ -2,31 +2,31 @@
 public class ClimbStairs {
 
     // -----way #1------------
-    // public int climbStairs(int n) {
-    // int[] dp = new int[n + 1];
-    // return helper(n, dp);
-    // }
-
-    // public int helper(int n, int[] dp) {
-    // if (n == 0 || n == 1) {
-    // return 1;
-    // }
-    // if (dp[n] != 0) {
-    // return dp[n];
-    // }
-    // return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
-    // }
-
-    // -------way # 2 ---------------
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+        return helper(n, dp);
     }
+
+    public int helper(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+    }
+
+    // -------way # 2 ---------------
+    // public int climbStairs(int n) {
+    // int[] dp = new int[n + 1];
+    // dp[0] = 1;
+    // dp[1] = 1;
+    // for (int i = 2; i <= n; i++) {
+    // dp[i] = dp[i - 1] + dp[i - 2];
+    // }
+    // return dp[n];
+    // }
 
     public static void main(String[] args) {
         ClimbStairs solution = new ClimbStairs();
