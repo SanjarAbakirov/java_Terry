22def36d3d984e7eab9decc68bb04f6e7be0b284
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-16 22:53:32 +0600

    auto commit Mon Mar 16 22:53:32 +06 2026

---
+++ java-foundations-solutions-master/src/model/Person.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+
+    public Person(String firstName, String lastName, LocalDate dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+    }
+}
