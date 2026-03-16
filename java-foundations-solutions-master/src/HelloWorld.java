065470b1034be7e2dda93779ef36372be8060d2e
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-16 23:40:04 +0600

    auto commit Mon Mar 16 23:40:04 +06 2026

--- java-foundations-solutions-master/src/HelloWorld.java
+++ java-foundations-solutions-master/src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! Nice to meet you!");
+
+        Person tom = new Person(firstName: "Tom", lastName: "Smith", LocalDate.of(1984, 6, 15));
     }
 }
